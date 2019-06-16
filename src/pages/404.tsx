6fa02321759517611cfc837aff978bd650303e70
Theo Browne <me@theo.li>
2019-06-16 04:29:03 -0700

    Added 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "./styles.css";
+
+const home = () => (
+  <div className="home">
+    <title>404</title>
+    <div className="page">
+      <div className="page-content">
+        <h1>404</h1>
+        <p>Not found</p>
+      </div>
+    </div>
+  </div>
+);
+
+export default home;
